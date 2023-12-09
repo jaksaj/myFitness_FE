8f01eab6e7916c0b15162180ecb643ba0b38ecd9
jaksaj <jaksaj35@gmail.com>
2023-12-09 19:21:01 +0100

    fixed errors

--- src/components/FormSignIn.jsx
+++ src/components/FormSignIn.jsx
@@ -2,96 +2,91 @@ import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 function FormSignIn() {
-    const [SignInData, setSignInData] = useState({
-        mail: "",
-        password: ""
-      });
-    const [isEmailValid, changeIsEmailValid] = useState(true);
-    const [isPasswordValid, changeIsPasswordValid] = useState(true);
-    function inputChange(event) {
-      const { name, value } = event.target;
-      setSignInData({ ...SignInData, [name]: value });
+  const [signInData, setSignInData] = useState({
+    email: "",
+    password: "",
+  });
+  const [isEmailValid, changeIsEmailValid] = useState(true);
+  const [isPasswordValid, changeIsPasswordValid] = useState(true);
+  function inputChange(event) {
+    const { name, value } = event.target;
+    setSignInData({ ...signInData, [name]: value });
+  }
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    validateInputs();
+    if (isEmailValid && isPasswordValid) {
+      signInUser(signInData);
     }
-    const handleSignIn = async (event) => {
-      event.preventDefault(); 
-      validateInputs();
-      if (isEmailValid && isPasswordValid) {
-        signInUser(SignInData);
-      }  
-    };
-    const validateInputs = () => {
-      const { mail, password } = SignInData;
-  
-      // Validate email
-      if (!mail.includes("@")) {
-        changeIsEmailValid(false);
+  };
+  const validateInputs = () => {
+    const { email, password } = signInData;
+
+    !email.includes("@") || email.length < 5
+      ? changeIsEmailValid(false)
+      : changeIsEmailValid(true);
+
+    password.length < 8
+      ? changeIsPasswordValid(false)
+      : changeIsPasswordValid(true);
+  };
+
+  const signInUser = async (userData) => {
+    console.log(userData);
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/users/login",
+        { userData }
+      );
+
+      if (response.status === 200) {
+        console.log("Log in complete!", response.data);
+        localStorage.setItem("token", response.data.token);
       } else {
-        changeIsEmailValid(true);
+        console.error("Error with sign in!", response.data);
       }
-  
-      // Validate password
-      if (password.length < 8) {
-        changeIsPasswordValid(false);
-      } else {
-        changeIsPasswordValid(true);
-      }
-    };
-    
-    const signInUser = async (userData) => {
-      try {
-        const response = await axios.post('http://localhost:3000', { userData });
-    
-        if (response.ok) {
-          
-          const responseData = await response.json();
-          console.log('Log in complete!', responseData);
-          localStorage.setItem('token', responseData.token);
-        } else {
-          
-          const errorData = await response.json();
-          console.error('Error with sign in!', errorData);
-        }
-      } catch (error) {
-        console.error('Error with sign in!', error);
-      }
-    };
-    return(
-      <>
-        <form onSubmit={handleSignIn}>
-          <div>
-            <label>Mail:
-              <input
-                type='text'
-                name='mail'
-                value={SignInData.mail}
-                onChange={inputChange}
-                required
-              />
-            </label>
-            <div style={{ color: 'red' }}>
-              {!isEmailValid && 'Email must contain @!'}
-            </div>
-            <label>Lozinka:
-              <input
-                type='text'
-                name='password'
-                value={SignInData.password}
-                onChange={inputChange}
-                required
-              />
-            </label>
-            <div style={{ color: 'red' }}>
-              {!isPasswordValid && 'Password can not be shorter then 8 units!'}
-            </div>
+    } catch (error) {
+      console.error("Error with sign in!", error);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSignIn}>
+        <div>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={signInData.email}
+              onChange={inputChange}
+              required
+            />
+          </label>
+          <div style={{ color: "red" }}>
+            {!isEmailValid && "This is not a valid email address"}
           </div>
-          
-          <button type="submit">Sign in!</button>
-        </form>
-        <p>
-          You do not have a account? <Link to={"/register"}>Register!</Link>
-        </p>
-      </>
-    )
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={signInData.password}
+              onChange={inputChange}
+              required
+            />
+          </label>
+          <div style={{ color: "red" }}>
+            {!isPasswordValid && "Password can not be shorter then 8 units!"}
+          </div>
+        </div>
+
+        <button type="submit">Sign in!</button>
+      </form>
+      <p>
+        You do not have a account? <Link to={"/register"}>Register!</Link>
+      </p>
+    </>
+  );
 }
 export default FormSignIn;
-
